fe8b4be0d2242fdd5384ead5dc525ad082ec3a3b
Ben Tefay <4169287+bentefay@users.noreply.github.com>
2018-11-16 17:09:34 +1000

    Complete FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -81,50 +81,44 @@ the contents of c
 
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
-printFile ::
-  FilePath
-  -> Chars
-  -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile :: FilePath -> Chars -> IO ()
+printFile filePath contents = do
+    putStrLn ("============ " ++ filePath)
+    putStrLn contents
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
-printFiles ::
-  List (FilePath, Chars)
-  -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles :: List (FilePath, Chars) -> IO ()
+printFiles = void . mapA (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
-getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile :: FilePath -> IO (FilePath, Chars)
+getFile filePath = (,) filePath <$> readFile filePath
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
-getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles :: List FilePath -> IO (List (FilePath, Chars))
+getFiles = mapA getFile
+
+mapA :: Applicative f => (a -> f b) -> List a -> f (List b)
+mapA f = sequence . (f <$>)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
-run ::
-  FilePath
-  -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run :: FilePath -> IO ()
+run filePath = do
+    filesPerLine <- readFile filePath
+    files <- getFiles $ lines filesPerLine
+    printFiles files
 
 -- /Tip:/ use @getArgs@ and @run@
-main ::
-  IO ()
-main =
-  error "todo: Course.FileIO#main"
+main :: IO ()
+main = do
+    args <- getArgs 
+    case args of
+        Nil -> error "Expected a file path"
+        (a :. _) -> run a
 
 ----
 
