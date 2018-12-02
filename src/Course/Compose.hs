d56d8348e8d2b4c61e0549d640a89775a0aaedd0
mattfurness <matt.furness@gmail.com>
2018-12-02 13:53:36 +1100

    Compose homework

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -8,28 +8,18 @@ import Course.Functor
 import Course.Applicative
 import Course.Monad
 
--- Exactly one of these exercises will not be possible to achieve. Determine which.
-
 newtype Compose f g a =
-  Compose (f (g a)) deriving (Show, Eq)
+  Compose { getCompose :: f (g a) }
 
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) f = Compose . (f <$$>) . getCompose
+    where
+      (<$$>) = (<$>) . (<$>)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure = Compose . pure . pure
+  (<*>) (Compose fgab) (Compose fga) = Compose $ (<*>) <$> fgab <*> fga
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
