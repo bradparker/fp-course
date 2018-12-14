b4e48391d05fb84f52622f6d741e03b770ba5eaf
James Ottaway <git@james.b12y.com>
2018-12-14 17:23:19 +1000

    FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -82,49 +82,41 @@ the contents of c
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
 printFile ::
-  FilePath
-  -> Chars
+  (FilePath, Chars)
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile (fp, c) = putStrLn $ "============ " ++ fp ++ "\n" ++ c
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = void . sequence . (<$>) printFile
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp = (\c -> pure (fp, c)) =<< readFile fp
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = sequence . (<$>) getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp = printFiles =<< (\c -> getFiles $ lines c) =<< readFile fp
 
 -- /Tip:/ use @getArgs@ and @run@
-main ::
-  IO ()
-main =
-  error "todo: Course.FileIO#main"
+main :: IO ()
+main = run =<< (pure . headOr "") =<< getArgs
 
 ----
 
