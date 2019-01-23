e3b41eae79017ec42bd5721f9feb0de03f399d8f
Dan Sowter <dan@stacktracehq.com>
2019-01-23 11:04:25 +1000

    Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -8,6 +8,7 @@ import Course.Core
 import Course.Functor
 import Course.Applicative
 import Course.Monad
+import Course.List
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
@@ -25,17 +26,18 @@ instance (Functor f, Functor g) =>
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
+  pure :: a -> Compose f g a
   pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+    Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (Compose fofGofAtoB) <*> (Compose fofGofA) =
+    Compose fofGofB
+    where
+      fofGofB = (<*>) <$> fofGofAtoB <*> fofGofA
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) :: (a -> Compose f g b) -> Compose f g a -> Compose f g b
-  aToCofFofB =<< (Compose fofGofA) = do
-    gOfA <- fofGofA
-    a <- gOfA
-    aToCofFofB a
+-- instance (Monad f, Monad g) =>
+--   Monad (Compose f g) where
+-- -- Implement the (=<<) function for a Monad instance for Compose
+--   (=<<) :: (a -> Compose f g b) -> Compose f g a -> Compose f g b
+--   (=<<) = error "Not possible."
